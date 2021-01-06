ff290ab3c2e9d2fa2afee6d4054f08bac971210e
アントムト <56132390+antomuto4@users.noreply.github.com>
2021-01-07 00:37:11 +0100

    Added: 4a.cpp

---
+++ 4a.cpp
@@ -0,0 +1,16 @@
+/* 
+Memory 8KB
+Time 60ms
+https://codeforces.com/contest/4/submission/103588549
+*/
+
+#include <iostream>
+int main(){
+int w; std::cin >> w;
+if(w % 2 == 0 && w > 2){
+std::cout << "YES" << std::endl;
+}
+else{
+std::cout << "NO" << std::endl;
+}
+}
