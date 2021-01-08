940d9d273439727e82ed2ee1042264b6d28fa29d
アントムト <56132390+antomuto4@users.noreply.github.com>
2021-01-08 17:31:09 +0100

    Create 50a.cpp

---
+++ cpp/50a.cpp
@@ -0,0 +1,15 @@
+/*
+Memory 8KB
+Time 62ms
+https://codeforces.com/contest/50/submission/103805033
+*/
+#include <iostream>
+int main(){
+int m, n, w;
+std::cin >> m;
+std::cin >> n;
+
+w = m*n/2;
+printf("%d", w);
+return 0;
+}
